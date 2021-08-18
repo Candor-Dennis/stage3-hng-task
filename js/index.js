4b17006e0a1608fb41f79cc9066f27ed2fef0b6a
candor-dennis <candordennis74@mail.com>
2021-08-18 21:39:47 +0100

    edited js file

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@
 let name = "Candor Dennis"
 let nameHolder = document.getElementById("name-holder")
 let nameDisplay = document.querySelector("h4")
+nameHolder.value = name;
 
 
 function print (){
-   nameHolder.value = name;
    nameDisplay.innerText = "My name is" + " " + name
    document.getElementById("show").classList.remove("d-none")
 }
